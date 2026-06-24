046c974ea25c40df5d693d9f54411f0db2775a4c
farhanchowdhury738 <farhanchowdhury738@gmail.com>
2026-06-24 22:52:19 +0600

    learn js variable

---
+++ JavaScript/first.js
@@ -0,0 +1,14 @@
+let age = 24;
+var name = "Farhan Chowdury";
+const pi = 3.14;
+
+
+console.log(age);
+console.log(name);
+console.log(pi);
+
+var num = 21;
+var num = 222;
+console.log(num)
+// That's why use let keyword!
+
