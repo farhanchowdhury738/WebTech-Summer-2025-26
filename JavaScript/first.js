56337af60951e497984f44a06becd15237f186c4
farhanchowdhury738 <farhanchowdhury738@gmail.com>
2026-07-05 18:17:48 +0600

    js loop learn

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -12,3 +12,30 @@ var num = 222;
 console.log(num)
 // That's why use let keyword!
 
+
+console.log(typeof(name));
+
+let x = null;
+
+
+// Object
+
+const student = {
+    name: "JavaScript",
+    age: 20,
+    isPass: true,
+}
+console.log(student.isPass);
+console.log(student["age"]);
+
+let score = 90
+if(score>80){
+    console.log("B+")
+}else if(score>85){
+    console.log("A")
+}
+
+for(let i=1; i<=5; i++){
+    console.log("Hello "+i);
+}
+ 
